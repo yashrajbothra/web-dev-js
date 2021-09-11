ce990bfdbfc22c278374972053b57c336d1d7f5d
Yash Raj Bothra <yashrajbothra786@gmail.com>
2021-09-11 15:49:50 +0530

    Updated Lec-11 Code

---
+++ lec-11/cookMaggi.js
@@ -0,0 +1,14 @@
+function cookMaggi(maggi, pani, tapeli) {
+    console.log("Your maggie will be ready in "
+        + maggi * 2
+        + " minutes"
+        + " and Ingredients used are:- "
+        + maggi + " Maggi "
+        + pani + " cups of water "
+        + "using " + tapeli + " pan")
+}
+
+cookMaggi(4 , 8 , 1);
+
+
+
